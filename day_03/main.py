c95863f2f5997909ca8c7f40411ec8b576332703
Alex <noelalexandre66@gmail.com>
2025-12-04 20:50:33 +0000

    Day 3: new approach! 0.76ms laptop

--- day_03/main.py
+++ day_03/main.py
@@ -2,51 +2,31 @@ from pathlib import Path
 
 HOME = Path(__file__).parent
 
-def combine(xs: list[int]) -> int:
-    res = 0
-    for x in xs:
-        res = res * 10 + x
-    return res
-
-ints = tuple(range(9, -1, -1))
+twelve = tuple(range(12))
 def best_combo(line: list[int]) -> int:
-    ll = len(line)
-    lim:int = ll - 11
-    for c in ints:
-        try:
-            start = line.index(c, 0, lim)
-            break
-        except: pass
-    
-    stack = [0]*12
-    p = 0
-    last = 0
-    for i in range(start, ll - 12):
-        n = line[i]
-        if p and last < n:
-            p -= 1
-            while p and stack[p-1] < n:
-                p -= 1
-            stack[p] = last = n
-            p += 1
-        elif p < 12:
-            stack[p] = last = n
-            p += 1
+    ll = len(line) # will be 100, but I like keeping the flexibility
+    lim = ll - 12
 
-    l = 0
-    for i in range(ll - 12, ll):
-        n = line[i]
-        if p > l and last < n:
-            p -= 1
-            while p > l and stack[p-1] < n:
-                p -= 1
-            stack[p] = last = n
-            p += 1
-        elif p < 12:
-            stack[p] = last = n
-            p += 1
-        l += 1
-    return combine(stack)
+    stack = 0
+    start = 0
+    c = 9
+    for _ in twelve:
+        # The limit has increased by 1 each time
+        # This means that it is sometimes possible to find a higher digit later on
+        # But this digit must be found in the 1 new position that has opened up
+        while c >= 0:
+            try:
+                start = line.index(c, start, lim + 1) + 1
+                break
+            except:
+                c -= 1
+        if line[lim] > c:
+            for i in line[lim:]: # using indexing is slower than direct slicing!
+                stack = stack * 10 + i
+            return stack
+        stack = stack * 10 + c
+        lim += 1
+    return stack
 
 data = [list(map(int,line.strip())) for line in (HOME/"input.txt").open()]
 
