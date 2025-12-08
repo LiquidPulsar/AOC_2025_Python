8513fdce2586575f7d309adf9c7060ae703f94a5
Alex <noelalexandre66@gmail.com>
2025-12-08 15:31:57 +0000

    Day 8: Timing

--- day_08/main.py
+++ day_08/main.py
@@ -2,26 +2,47 @@ from heapq import nlargest
 from math import prod, dist
 from pathlib import Path
 from scipy.cluster.hierarchy import DisjointSet
+from time import perf_counter_ns
 
 HOME = Path(__file__).parent
 
 with open(HOME/"input.txt") as f:
     points:list[tuple[int,int,int]] = [*map(eval,f)]
 
-
-sets = DisjointSet(points)
-
+start = perf_counter_ns()
 distances = sorted(
     (dist(p1, p2), p1, p2)
     for i,p1 in enumerate(points)
     for p2 in points[i+1:]
 )
+end = perf_counter_ns()
+print(f"Distance calc took {(end-start)/1_000_000} ms")
 
-for i,(_,a,b) in enumerate(distances):
-    sets.merge(a,b)
-    if i == 1000:
-        print(prod(nlargest(3, sets._sizes.values())))
+def solve(points: list[tuple[int,int,int]]) -> tuple[int,int]:
+    sets = DisjointSet(points)
 
-    if sets.n_subsets == 1:
-        print(a[0]*b[0])
-        break
+    distances = sorted(
+        (dist(p1, p2), p1, p2)
+        for i,p1 in enumerate(points)
+        for p2 in points[i+1:]
+    )
+
+    s = 0
+    for i,(_,a,b) in enumerate(distances):
+        sets.merge(a,b)
+        if i == 1000:
+            s = prod(nlargest(3, sets._sizes.values()))
+
+        if sets.n_subsets == 1:
+            return s, a[0]*b[0]
+    assert False, "Shouldn't reach here"
+
+N_RUNS = 10
+total = 0
+res = None
+for _ in range(N_RUNS):
+    start = perf_counter_ns()
+    res = solve(points)
+    end = perf_counter_ns()
+    total += end - start
+print(f"Solution took {total/N_RUNS/1_000_000} ms, result {res}")
