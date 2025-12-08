9ff89733474e8d317f3bba69c7bccf7c60573702
Alex <noelalexandre66@gmail.com>
2025-12-08 16:23:54 +0000

    Day 8: super speedy shortest_dists (algorithmically, raw speed unoptimised save for prefetch dist), 22.4ms laptop

--- day_08/main.py
+++ day_08/main.py
@@ -1,43 +1,72 @@
-from heapq import nlargest
-from math import prod, dist
+from heapq import nlargest, heapify, heappop, heappush
+from math import prod
 from pathlib import Path
+from itertools import count, starmap
+from scipy.spatial import KDTree
 from scipy.cluster.hierarchy import DisjointSet
 from time import perf_counter_ns
 
 HOME = Path(__file__).parent
 
-with open(HOME/"input.txt") as f:
-    points:list[tuple[int,int,int]] = [*map(eval,f)]
+with open(HOME / "input.txt") as f:
+    points: list[tuple[int, int, int]] = [*map(eval, f)]
 
-start = perf_counter_ns()
-distances = sorted(
-    (dist(p1, p2), p1, p2)
-    for i,p1 in enumerate(points)
-    for p2 in points[i+1:]
-)
-end = perf_counter_ns()
-print(f"Distance calc took {(end-start)/1_000_000} ms")
+N = 18 # Prefetch distance
+def shortest_dists(pos):
+    # Returns generator of (distance, i, j) in order of increasing distance
+    # O(log n) per yielded distance
+    tree = KDTree(pos)
+    distheap = []
 
-def solve(points: list[tuple[int,int,int]]) -> tuple[int,int]:
-    sets = DisjointSet(points)
+    # For a given point, generate its nearest neighbours in order
+    def gen_ns(i, p):
+        for k in count(2):
+            ds, inds = tree.query(p, k=[*range(k, k+N)])  # type: ignore (scipy stubs bad)
+            for j in range(N):
+                yield ds[j], i, int(inds[j])  # type: ignore
 
-    distances = sorted(
-        (dist(p1, p2), p1, p2)
-        for i,p1 in enumerate(points)
-        for p2 in points[i+1:]
-    )
+    gens = [*starmap(gen_ns, enumerate(pos))]
+    distheap = [*map(next, gens)]  # Prime with first neighbour of each point
+
+    heapify(distheap)
+    while distheap:
+        distance, i, j = heappop(distheap) # O(log n)
+        if j > i:  # dedupe
+            yield distance, i, j
+        next_neighb = next(gens[i])  # Get next neighbour for point i, O(log n) amortized
+        if next_neighb[0] < 1e100:
+            heappush(distheap, next_neighb)
+
+# from collections import deque
+# from itertools import islice
+# consumer = deque(maxlen=0)
+# for N in range(15, 26):
+#     N_RUNS = 1000
+#     total = 0
+#     for _ in range(N_RUNS):
+#         start = perf_counter_ns()
+#         consumer.extend(islice(shortest_dists(points), 4603)) # By inspection, we need 4602 edges for my input
+#         end = perf_counter_ns()
+#         total += end - start
+#     print(f"Solution {N} took {total/N_RUNS/1_000_000} ms)
+
+
+def solve(points: list[tuple[int, int, int]]) -> tuple[int, int]:
+    sets = DisjointSet(range(len(points)))
+
+    distances = shortest_dists(points)
 
     s = 0
-    for i,(_,a,b) in enumerate(distances):
-        sets.merge(a,b)
+    for i, (_, j, k) in enumerate(distances):
+        sets.merge(j, k)
         if i == 1000:
             s = prod(nlargest(3, sets._sizes.values()))
 
         if sets.n_subsets == 1:
-            return s, a[0]*b[0]
+            return s, points[j][0] * points[k][0]
     assert False, "Shouldn't reach here"
 
-N_RUNS = 10
+N_RUNS = 100
 total = 0
 res = None
 for _ in range(N_RUNS):
@@ -45,4 +74,4 @@ for _ in range(N_RUNS):
     res = solve(points)
     end = perf_counter_ns()
     total += end - start
-print(f"Solution took {total/N_RUNS/1_000_000} ms, result {res}")
+print(f"Solution {N} took {total/N_RUNS/1_000_000} ms, result {res}")
